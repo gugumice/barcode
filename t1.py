c129d1c5c596bca75115e2cd15adc147120ea4e0
Gunars Frickauss <gunarsf@gmail.com>
2020-01-08 07:09:33 +0200

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,33 +1,42 @@
-
-#!/usr/bin/python3
-import sys
+#!/usr/bin/env python
+from queue import Queuequeue.LifoQueue
+from threading import Thread, Event
 from time import sleep
-from threading import Thread
-
-class FileLoaderThread(Thread):
-   def __init__(self, fileName, encryptionType):
-       # Call the Thread class's init function
-       Thread.__init__(self)
-       self.fileName = fileName
-       self.encryptionType = encryptionType
-
-   # Override the run() function of Thread class
-   def run(self):
-       print('Started loading contents from file : ', self.fileName)
-       print('Encryption Type : ', self.encryptionType)
-       for i in range(5):
-           print('Loading ... ')
-           sleep(1)
-       print('Finished loading contents from file : ', self.fileName)
-
-th = FileLoaderThread('users.csv','ABC')
-th.start()
-
-# print some logs in main thread
-for i in range(5):
-   print('Hi from Main Function')
-   sleep(1)
-
-# wait for thread to finish
-th.join()
-
+from random import randint
+# A thread that produces data
+def producer(out_q):
+    while True:
+        data=randint(1,51)
+        print(data)
+        sleep(.2)
+        # Make an (data, event) pair and hand it to the consumer
+        if data == 5:
+            print('data sent {}'.format(data))
+            evt = Event()
+            out_q.put((data, evt))
+            # Wait for the consumer to process the item
+            evt.wait()
+# A thread that consumes data
+def consumer(in_q):
+    print('consumer waiting')
+    while True:
+        # Get some data
+        data, evt = in_q.get()
+        # Process the data
+        
+        print('Data received {}'.format(data))
+        sleep(5)
+        # Indicate completion
+        evt.set()
+def main():
+    #q = Queue.LifoQueue(maxsize=1)
+    q = Queue.LifoQueue()
+    t1 = Thread(target=consumer, args=(q,))
+    t2 = Thread(target=producer, args=(q,))
+    t1.start()
+    t2.start()
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
